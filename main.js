b351f919805dd0ca4f5bd1b0b96b78ae0f15b918
Courtney Sherrod <courtneylamontsherrod@gmail.com>
2020-06-19 14:02:07 -0500

    https://github.com/virgocourt1970/dice_roller.gi updating changes

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log ("hi")
+const rollbutton = document.querySelector('#roll-button')
+const dieRolls = []
+
+rollbutton.addEventListener("click", function () {
+    console.log ("roll button click")
+    //loop a number of times equal to the number-of-dice
+})
