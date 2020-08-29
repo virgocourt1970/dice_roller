7f6b7ec825690a610cea11fb9215e963e0f16aef
Courtney Sherrod <courtneylamontsherrod@gmail.com>
2020-08-29 11:16:18 -0500

    added changes to file

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-console.log ("hi")
-const rollbutton = document.querySelector('#roll-button')
-const dieRolls = []
+console.log("hi");
+const rollbutton = document.querySelector("#roll-button");
+const diceinput = document.querySelector("#numberofdiceused");
+const totaldiv = document.querySelector("#total");
+const dieRolls = [];
 
 rollbutton.addEventListener("click", function () {
-    console.log ("roll button click")
-    //loop a number of times equal to the number-of-dice
-})
+  let dicevalue = diceinput.value;
+  let total = 0;
+  for (let i = 0; i < dicevalue; i = i + 1) {
+    let diceroll = Math.ceil(Math.random() * 6);
+    console.log(diceroll);
+    total += diceroll;
+    dieRolls.push(diceroll);
+  }
+  console.log(total);
+  totaldiv.innerHTML = total;
+});
